dcacc16e7d7679dca05105b5069220ac4118b006
risonakamo <hmykanae@gmail.com>
2020-10-18 13:15:22 -0400

    loading input links and switching to tile mode

--- link-opener/link-open-index.tsx
+++ link-opener/link-open-index.tsx
@@ -1,5 +1,6 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useRef} from "react";
 import ReactDOM from "react-dom";
+import cx from "classnames";
 import _ from "lodash";
 
 import LinkTile from "./components/link-tile/link-tile";
@@ -17,6 +18,11 @@ function LinkOpenMain():JSX.Element
 {
   // the currently loaded links
   const [theCurrentLinks,settheCurrentLinks]=useState<BookmarkTileInfo[]>([]);
+
+  // if in tile view or not.
+  const [tileMode,setTileMode]=useState<boolean>(false);
+
+  const theLinkInputBox=useRef<HTMLTextAreaElement>(null);
 
   // set the current links. also shuffles them.
   function loadLinks(links:string[]):void
@@ -69,6 +75,19 @@ function LinkOpenMain():JSX.Element
     openAllTabs(selectedUrls);
   }
 
+  // attempt to load the input and switch to tiles view with the input
+  function loadInput():void
+  {
+    var inputedLinks:string[]=_.filter(_.map(theLinkInputBox.current!.value.split("\n"),(x:string)=>{
+      return x.trim();
+    }),(x:string)=>{
+      return x.length;
+    }) as string[];
+
+    loadLinks(inputedLinks);
+    setTileMode(true);
+  }
+
   var selectedCount:number=0;
   const generatedLinkTiles:JSX.Element[]=_.map(theCurrentLinks,(x:BookmarkTileInfo,i:number)=>{
     if (x.selected)
@@ -80,16 +99,20 @@ function LinkOpenMain():JSX.Element
       selected={x.selected} inactive={x.inactive}/>;
   });
 
+  const contentAreaClass={
+    "tile-mode":tileMode
+  };
+
   return <>
     <div className="tool-bar">
       <span className="selected-count">{selectedCount} selected</span>
       <button onClick={openLinks}>open selected links</button>
       <button onClick={shuffleLinks}>shuffle</button>
     </div>
-    <div className="content-area">
+    <div className={cx("content-area",contentAreaClass)}>
       <div className="links-input-zone">
-        <textarea className="link-input"></textarea>
-        <button>submit</button>
+        <textarea className="link-input" ref={theLinkInputBox}></textarea>
+        <button onClick={loadInput}>submit</button>
       </div>
       <div className="link-tiles">
         {generatedLinkTiles}
